4af596a3ffa663e601643e29d367e76a7cefd5bf
Costalferz <costalferzus@gmail.com>
2022-08-14 03:31:21 +0700

    update spyder

--- tutorial/spiders/quote_spyder.py
+++ tutorial/spiders/quote_spyder.py
@@ -4,20 +4,22 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = ['https://www.dataforthai.com/business/objective',]
+    def parse(self, response):
+        for i in response.css('tr.click-able-row a::attr(href)'):
+            url = response.urljoin(i.extract())
+            yield scrapy.Request(url, callback=self.parse_data)
+            self.logger.info('Processing %s', response.url)
 
-    def parse_page1(self, response):
-        for i in response.css('tr.click-able-row a::attr(href)').getall():
-            return scrapy.Request("https://www.dataforthai.com"+i,callback=self.parse_page2)
-
-    def parse_page2(self, response):
-        for quote in response.xpath('//*[@id="datatable"]/tbody/tr'):
+    def parse_data(self, response):
+         for quote in response.xpath('//*[@id="datatable"]/tbody/tr'):
             yield {
-                'entrepreneur': quote.xpath('td[2]/text()').get(),
-                'capital': quote.xpath('td[3]/text()').get(),
-                'amphoe': quote.xpath('td[5]/text()').get(),
-                'tambon': quote.xpath('td[6]/text()').get(),
-            }
-        self.logger.info("Visited %s", response.url)
+            'entrepreneur': quote.xpath('td[2]/text()').get(),
+            'categories': quote.xpath('//*[@id="main"]/div[4]/div[1]/h1/text()').get().replace(' รายชื่อผู้ประกอบการกลุ่มธุรกิจ ', ''),
+            'capital': quote.xpath('td[3]/text()').get(),
+            'amphoe': quote.xpath('td[5]/text()').get(),
+            'tambon': quote.xpath('td[6]/text()').get(),
+         }
+
         # def first_page(self, response):
         # href = response.css('tr.click-able-row a::attr(href)').getall()
         # for i in href:
