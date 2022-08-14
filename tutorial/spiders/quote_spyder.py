4ec07f02654b47661a467f5692e0ca17cb34f6a7
Costalferz <costalferzus@gmail.com>
2022-08-14 14:05:41 +0700

    update code

--- tutorial/spiders/quote_spyder.py
+++ tutorial/spiders/quote_spyder.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from datetime import datetime
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
@@ -14,15 +14,11 @@ class QuotesSpider(scrapy.Spider):
          for quote in response.xpath('//*[@id="datatable"]/tbody/tr'):
             yield {
             'entrepreneur': quote.xpath('td[2]/text()').get(),
-            'categories': quote.xpath('//*[@id="main"]/div[4]/div[1]/h1/text()').get().replace(' รายชื่อผู้ประกอบการกลุ่มธุรกิจ ', ''),
             'capital': quote.xpath('td[3]/text()').get(),
             'amphoe': quote.xpath('td[5]/text()').get(),
             'tambon': quote.xpath('td[6]/text()').get(),
+            'categories': quote.xpath('//*[@id="main"]/div[4]/div[1]/h1/text()').get().split(' ')[2],
+            'code': response.url[-5:],
+            'date': datetime.now().strftime("%Y-%m-%d-%T"),
          }
 
-        # def first_page(self, response):
-        # href = response.css('tr.click-able-row a::attr(href)').getall()
-        # for i in href:
-        #     url = 'https://www.dataforthai.com'+i
-        #     self.logger.info("Parsing match on url: " + url)
-        #     yield response.Request(url=url, callback=self.parse)
