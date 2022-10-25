c1add501d03e87d19ed612ae9f5dbfa2344ef6e8
costalferz <costalferzus@gmail.com>
2022-10-25 12:55:42 +0700

    update data

--- tutorial/spiders/evdatabase.py
+++ tutorial/spiders/evdatabase.py
@@ -4,22 +4,17 @@ from datetime import datetime
 class QuotesSpider(scrapy.Spider):
     name = "evbase"
     start_urls = ['https://ev-database.org',]
+
     def parse(self, response):
-        for i in response.css('div.data-wrapper'):
-            yield{
-                'brand': i.css('h2::text').get(),
+        
+        for i in response.xpath('.//div[@class="data-wrapper"]'):
+            yield {
+                "brand" : i.css('span::text').get(),
+                "model" : i.css('span.model::text').get(),
+                "seat" : i.xpath('.//*[@title="Number of seats"]/text()').get(),
+                "battery" : i.css('span.battery::text').get(),
+                "range" : i.css('span.erange_real::text').get(),
+                "topspeed": i.css('span.topspeed::text').get(),
+                "acceleration": i.css('span.acceleration::text').get(),
+
             }
-            # yield scrapy.Request(url, callback=self.parse_data)
-
-    # def parse_data(self, response):
-    #      for quote in response.xpath('//*[@id="datatable"]/tbody/tr'):
-    #         yield {
-    #         'entrepreneur': quote.xpath('td[2]/text()').get(),
-    #         'capital': quote.xpath('td[3]/text()').get(),
-    #         'amphoe': quote.xpath('td[5]/text()').get(),
-    #         'tambon': quote.xpath('td[6]/text()').get(),
-    #         'categories': quote.xpath('//*[@id="main"]/div[4]/div[1]/h1/text()').get().split(' ')[2],
-    #         'code': response.url[-5:],
-    #         'date': datetime.now().strftime("%Y-%m-%d-%T"),
-    #      }
-
