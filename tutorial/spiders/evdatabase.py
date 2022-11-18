7e37e33fc7a72d00e6e8e984ce1ad4403c0c8354
costalferz <costalferzus@gmail.com>
2022-11-18 13:44:05 +0700

    update column

--- tutorial/spiders/evdatabase.py
+++ tutorial/spiders/evdatabase.py
@@ -9,12 +9,20 @@ class QuotesSpider(scrapy.Spider):
         
         for i in response.xpath('.//div[@class="data-wrapper"]'):
             yield {
-                "brand" : i.css('span::text').get(),
-                "model" : i.css('span.model::text').get(),
-                "seat" : i.xpath('.//*[@title="Number of seats"]/text()').get(),
-                "battery" : i.css('span.battery::text').get(),
-                "range" : i.css('span.erange_real::text').get(),
-                "topspeed": i.css('span.topspeed::text').get(),
-                "acceleration": i.css('span.acceleration::text').get(),
+                "Brand" : i.css('span::text').get(),
+                "Model" : i.css('span.model::text').get(),
+                "Seat" : i.xpath('.//*[@title="Number of seats"]/text()').get(),
+                "Battery" : i.css('span.battery::text').get(),
+                "Range" : i.css('span.erange_real::text').get(),
+                "Topspeed": i.css('span.topspeed::text').get(),
+                "Fastcharge": i.css('span.fastcharge_speed_print::text').get(),
+                "Acceleration": i.css('span.acceleration::text').get(),
+                "Tow_Capacity": i.xpath('.//*[@title="Towing capacity in kg"]/text()').get(),
+                "Price": i.xpath('//*[@id="evdb"]/main/div[2]/div[3]/div[2]/div/div[3]/span[4]/@title').get(),
+                "Price_GE": i.css('span.country_de::text').get(),
+                "Price_Nl": i.css('span.country_nl::text').get(),
+                "Price_UK": i.css('span.country_uk::text').get(),
+                "Market_Segment": i.xpath('.//*[@title="Market Segment"]/text()').get(),
+
 
             }
